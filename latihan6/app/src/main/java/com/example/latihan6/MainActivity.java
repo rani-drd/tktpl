7af4debd8df3e098bdda35852a50553005a6df0c
rani <rani@drdaccess.com>
2019-11-28 12:29:20 +0700

    init project with wifi configutation

---
+++ latihan6/app/src/main/java/com/example/latihan6/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.latihan6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiManager;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        wifiSetup();
+    }
+
+    public void wifiSetup() {
+        String networkSSID = "HotSpot-UI";
+//        no password
+//        String networkPass = "pass";
+
+        WifiConfiguration conf = new WifiConfiguration();
+        conf.SSID = "\"" + networkSSID + "\"";
+//
+//        WEP Network?
+//        conf.wepKeys[0] = "\"" + networkPass + "\"";
+//        conf.wepTxKeyIndex = 0;
+//        conf.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.NONE);
+//        conf.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.WEP40);
+
+//        WPA Network
+//        conf.preSharedKey = "\""+ networkPass +"\"";
+
+//        Open Network
+        conf.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.NONE);
+
+        WifiManager wifiManager = (WifiManager)getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        int netId = wifiManager.addNetwork(conf);
+        wifiManager.disconnect();
+        wifiManager.enableNetwork(netId, true);
+        wifiManager.reconnect();
+
+
+    }
+}
